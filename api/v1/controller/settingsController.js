826e52a102b035d290eee8d7fcbd4bba6a5e4b1a
Lena Kwan <lenakwan@gmail.com>
2021-02-04 00:13:36 -0800

    edited settingsController

--- api/v1/controller/settingsController.js
+++ api/v1/controller/settingsController.js
@@ -5,10 +5,11 @@ const settingsModel = require('../model/settingsModel');
 createSetting = async (req, res) => {
     let body = req.body;
     settingsModel.createSetting(body.id, body.category, body.name, body.user_status, body.bg_img, body.bg_link,
-        body.bg_color, body.header_bg_img, body.header_bg_link, body.header_bg_color, body.profile_img, body.content_color, body.pricing).catch(err => res.status(500).json({
-        message: "Error 500 Internal Server Error: " + err.message
-    }))
-    res.status(200).json('User setting added to the Database');
+        body.bg_color, body.header_bg_img, body.header_bg_link, body.header_bg_color, body.profile_img, body.content_color, body.pricing).then((data)=>{
+            res.status(200).json('User Setting added to Database');
+        }).catch(err => res.status(500).json({
+            message: "Error 500 Internal Server Error: " + err.message
+        }));
 }
 
 module.exports = {
